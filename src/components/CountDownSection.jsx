cf2e61c1cce9494d8d1eaf9d4207e4f2692197ce
Assayyaad <hi@thealpha.pro>
2024-10-24 14:18:36 +0800

    إرجاع الموقع بعد تحديثه لأسفل العد التنازلي

--- src/components/CountDownSection.jsx
+++ src/components/CountDownSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaExternalLinkAlt, FaMapMarkerAlt } from 'react-icons/fa'
 import Countdown from './CountDown.jsx'
 import Register from './Register.jsx'
 import { countDown } from '../db.js'
@@ -15,6 +16,22 @@ export default function CountDownSection() {
         </header>
         <div dir="ltr">
           <Countdown targetDate={targetDate} />
+        </div>
+        <div className="flex flex-col items-center text-center justify-center">
+          <a
+            href={countDown.maps}
+            className="px-2 py-2 rounded-lg shadow-md bg-[#fff] hover:bg-[#ccc] duration-300 text-black"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="flex items-center justify-center gap-1 mb-2">
+              <FaExternalLinkAlt />
+              <FaMapMarkerAlt />
+            </div>
+            {countDown.location.map((line, i) => (
+              <p key={i}>{line}</p>
+            ))}
+          </a>
         </div>
         <Register />
       </div>
