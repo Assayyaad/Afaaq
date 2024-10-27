13bf95732ee8d7e9d859b948068178e6dd57f88f
akashi-miano <abdulsalam2041@outlook.com>
2024-10-27 14:43:05 +0800

    وسطت النص في ال Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className="hero grid items-center justify-center">
       <div className="container text-white">
-        <header className="flow-content--m">
+        <header className="flow-content--m text-center">
           <h1 className="title font-bold text-4xl text-center" data-aos="fade-right" data-aos-delay="100">
             {desc.title}
           </h1>
